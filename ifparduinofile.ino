7b6cf16c3b1c18df53a75e330fcbfdd4a27a00cf
rino <rinovethamoses97@gmail.com>
2018-08-22 09:21:32 +0530

    Initial Commit

---
+++ ifparduinofile.ino
@@ -0,0 +1,144 @@
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * Example sketch/program showing how to read data from a PICC to serial.
+ * --------------------------------------------------------------------------------------------------------------------
+ * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+ * 
+ * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+ * Reader on the Arduino SPI interface.
+ * 
+ * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+ * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+ * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+ * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
+ * when removing the PICC from reading distance too early.
+ * 
+ * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
+ * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
+ * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
+ * keep the PICCs at reading distance until complete.
+ * 
+ * @license Released into the public domain.
+ * 
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP8266WiFi.h>
+const char* ssid  = "SSN";
+const char* password = "Ssn1!Som2@Sase3#";
+const char* host = "10.112.150.144";
+const char* passcode = "YOUR_PASSCODE";
+constexpr uint8_t RST_PIN =D3; //2;          // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = D4;//0;         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+   String id;
+   if (mfrc522.PICC_IsNewCardPresent()) { // (true, if RFID tag/card is present ) PICC = Proximity Integrated Circuit Card
+    if(mfrc522.PICC_ReadCardSerial()) { // true, if RFID tag/card was read
+      Serial.print("RFID TAG ID:");
+      
+      
+      for (byte i = 0; i < mfrc522.uid.size; ++i) { // read id (in parts)
+        //Serial.print(mfrc522.uid.uidByte[i]); // print id as hex values
+        id+=mfrc522.uid.uidByte[i];
+        //Serial.print(" "); // add space between hex blocks to increase readability
+      }
+      Serial.println(id); // Print out of id is complete.
+      
+
+
+      //inserting data part
+     WiFiClient client;
+     const int httpPort = 81;
+     if (!client.connect(host, httpPort)) {
+       Serial.println("Connection failed!");
+        return;
+      }
+      String url = "/arduino.php?s1=";
+      url += id;
+
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+//  unsigned long timeout = millis();
+//  while (client.available() == 0) {
+//    if (millis() - timeout > 5000) {
+//      Serial.println(">>> Client Timeout !");
+//      client.stop();
+//      return;
+//    }
+//  }
+
+
+
+
+
+
+
+
+
+
+
+      id="";
+      //inserting data ends 
+    }
+  }
+  else{
+     
+       if (!mfrc522.PICC_IsNewCardPresent()) {
+       WiFiClient client;
+      const int httpPort = 81;
+      if (!client.connect(host, httpPort)) {
+        Serial.println("Connection failed!");
+        return;
+      }
+      String url = "/arduino.php?s1=empty";
+
+
+       Serial.print("Requesting URL: ");
+       Serial.println(url);
+       client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+     }
+ }
+}
